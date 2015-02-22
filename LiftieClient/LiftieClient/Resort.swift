d7e1d607646bab411a7b48fc02fdb92635397eb2
Gary Shirk <gary@mfluent.com>
2015-02-22 10:06:06 -0600

    initial master detail vc commit

---
+++ LiftieClient/LiftieClient/Resort.swift
@@ -0,0 +1,17 @@
+//
+//  Resort.swift
+//  LiftieClient
+//
+//  Created by Gary Shirk on 2/22/15.
+//  Copyright (c) 2015 garyshirk. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Resort: NSManagedObject {
+
+    @NSManaged var id: String?
+    @NSManaged var name: String?
+
+}
